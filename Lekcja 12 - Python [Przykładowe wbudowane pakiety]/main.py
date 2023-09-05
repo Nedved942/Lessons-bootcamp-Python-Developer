60cb7c452965352d48c00a961956f5b53f8679ee
nedved942 <pawelkoziol77@wp.pl>
2023-09-05 20:20:33 +0200

    Utworzenie lekcji 12

---
+++ Lekcja 12 - Python [Przykładowe wbudowane pakiety]/main.py
@@ -0,0 +1,49 @@
+"""
+Przykładowe wbudowane pakiety:
+- math
+- random
+- copy (jedna z najważniejszych)
+- pprint
+- sys
+- os
+- csv
+
+"""
+
+#
+# import random
+#
+# randomized_integer = random.randint(4, 8)
+#
+# random_element_from_range = random.randrange(0, 100, 4)  # Losuje element od 0 do 100, ale "skacze" co 4 elementy
+# print(random_element_from_range)
+#
+#
+#
+# """
+# Copy
+# """
+#
+# import copy
+# some_dict = {"aa": 12}
+# copied_dict
+#
+
+"""
+pprint - "pretty print"
+"""
+
+# from pprint import
+
+
+"""
+sys
+"""
+
+import sys
+
+print(sys.platform)  # System operacyjny
+print(sys.path)  # Ścieżka
+print(sys.argv)  # Argumenty wejściowe dla skryptu Pythona
+print(sys.api_version)
+size_of_very_large_complex_dict = sys.getsizeof(very_large_complex_dict)
