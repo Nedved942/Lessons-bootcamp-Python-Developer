4f2ba87893eb81ef59d77f9ec078b8695093f3fb
nedved942 <pawelkoziol77@wp.pl>
2023-09-12 17:39:12 +0200

    Modyfikacja pytań i #Do konsultacji i dodatnie pliku .gitignore

---
+++ Lekcja 14 - Venv, i pip, skrótowe operacje na dict/main.py
@@ -0,0 +1,11 @@
+"""Debugger"""
+
+# integer_value_as_text = input("Podaj wartość liczbową: ")
+# integer_value = int(integer_value_as_text)
+# print(integer_value)
+#
+# # Odpalenie kodu za pomocą debuggera 'Shift + F9'
+
+"""Wirtualne środowisko"""
+
+import requests
