8bc67249bc87a4c01857a791ef0c043f067c4033
nedved942 <pawelkoziol77@wp.pl>
2023-10-04 21:59:12 +0200

    Aktualizacja pliku Pytania i konsultacje

---
+++ Lekcja 20 - Dekoratory, SOLID/main.py
@@ -0,0 +1,12 @@
+def say_hello():
+    print("Hello!")
+
+
+print(say_hello)  # Odwołujemy się do obiektu funkcji (miejsca gdzie jest ulokowana w pamięci)
+
+
+# cache - dekorator
+# Po wykonaniu operacj, jej wynik jest zapamiętywany w pamięci
+
+
+# functools - biblioteka, zbiór różnych pythonowych funkcjonalności nieskategoryzowanych
