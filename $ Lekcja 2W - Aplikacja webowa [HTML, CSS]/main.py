19e999a29134aeccb16d9da537d048d9bc2f0d2d
nedved942 <pawelkoziol77@wp.pl>
2023-10-12 21:15:56 +0200

    Dodanie lekcji 2 z aplikacji webowej i mmodyfikacja pliku z pytaniami i skrótów

---
+++ $ Lekcja 2W - Aplikacja webowa [HTML, CSS]/main.py
@@ -0,0 +1,12 @@
+"""
+jinja2 - silnik tłumaczenia z pythona do pliku HTML
+
+W HTML trzeba pamiętać o 'endforze', 'endifie', itp., ponieważ Python rozrużnia zakres (scope) operacji poprzez wcięcia,
+natomiast w HTML (i dużo innych językach programowania) trzeba zaznaczyć zakończenie pewnego zakresu opreacji.
+
+query parameters - parametry wysłane w linku
+
+
+"""
+
+
