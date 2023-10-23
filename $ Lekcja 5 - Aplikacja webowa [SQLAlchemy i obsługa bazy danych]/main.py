36de8688665f4cdca31b026ed7d9b975b3a4b811
nedved942 <pawelkoziol77@wp.pl>
2023-10-23 19:54:23 +0200

    Dodanie Lekcji 5 z aplikacji webowej

---
+++ $ Lekcja 5 - Aplikacja webowa [SQLAlchemy i obsługa bazy danych]/main.py
@@ -0,0 +1,37 @@
+"""
+Do jakiego typu programów należy Bootstrap?
+
+Jedna tabela reprezentuje rodzaj danych
+Rzędy to jest ilość wpisów
+Każda kolumna przechowuje jakiś typ danych
+
+Każda tabela musi mieć parametr ID, która wyznacza liczbę pozycyjną wpisów
+
+
+Nie mamy potrzeby pracować na czystym SQLu, ponieważ możemy pracować na narzędziu ORM - Object Related Mapping
+Pythonowa składnia, która pozwala na definiowanie połączeń i tego jakie operacje wykonujemy na bazie danych
+
+Przy rekrutacji: czym to jest i do czego można tego użyć przynajmniej wiedzieć.
+
+flask-sqlalchemy - Paczka do operacji na bazach danych poprzez Pythona (To jest ORM)
+
+
+.db - database
+
+Ścieżka relatywna i ścieżka absolutna
+
+Do googlowania SQLAlchemy, bazy danych, pocżątki baz danych, SQL, migracje
+Funkcja dodawania użytkownika
+
+
+DB Browser - narzędzie do przeglądania baz danych
+W momencie jak jest otwarta baza danych w tym programie to nie można jej modyfikować. Trzeba zamknąć i otworzyć.
+
+Pierwszą kolumną, którą zawsze definiujemy to jest "id"
+
+
+migracje
+
+
+skrót usuwanie przed kursorem !
+"""
