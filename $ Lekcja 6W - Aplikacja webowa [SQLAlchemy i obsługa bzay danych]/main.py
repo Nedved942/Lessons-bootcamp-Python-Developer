6ead1d59fadabf4d85044aa2c99a7f4755697edc
nedved942 <pawelkoziol77@wp.pl>
2023-10-28 19:40:47 +0200

    Dodanie lekcji 6 z Aplikacji webowej

---
+++ $ Lekcja 6W - Aplikacja webowa [SQLAlchemy i obsługa bzay danych]/main.py
@@ -0,0 +1,7 @@
+""""
+Szukanie błędów - możesz skopiować frazę pliku lub zmiennej i wyszukujesz na projekcie Ctrl + Shift + F (Find in Files)
+
+Relacje pomiędzy tabelami - ( w tym przypadku dwiema tabelami )
+
+Połączenie zwrotne.
+"""
